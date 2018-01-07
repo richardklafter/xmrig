e488d777715f371cc96c764d82bc406ec34b8b7d
Richard Klafter <richard@zinc.com>
2018-01-07 00:01:25 -0800

    update URL

--- build/run.sh
+++ build/run.sh
@@ -4,5 +4,5 @@ NUMTHREADS=$(python -c "from math import floor; print int(floor($NUMCPU*.60))")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 echo "Detected $NUMCPU cores running with $NUMTHREADS threads"
-exec ./xmrig -t "$NUMTHREADS" -o stratum+tcp://xmr.pool.minergate.com:45560 -u rpklafter@yahoo.com -p x
+exec ./xmrig -t "$NUMTHREADS" -o stratum+tcp://xdn-xmr.pool.minergate.com:45560 -u rpklafter@yahoo.com -p x
 
