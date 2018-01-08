c19a29d9ade000b048fda70cb72320d246f4e25d
Richard Klafter <richard@zinc.com>
2018-01-08 01:17:50 -0800

    bug fix

--- scripts/cputool_run.sh
+++ scripts/cputool_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-CURRENT_LOAD_AVG=$(cat /proc/loadavg | awk '{print $3}') #load avg of last 10 mins
+echo "Sleeping 60s to collect current load average..."
+sleep 61
+CURRENT_LOAD_AVG=$(cat /proc/loadavg | awk '{print $1}') #load avg of last 10 mins
 NUMBER_CPUS=$(grep -c ^processor /proc/cpuinfo)
 NUMBER_OF_THREADS=$(python -c "from math import floor; print(int(floor($NUMBER_CPUS-$CURRENT_LOAD_AVG-1)))")
 LOAD_LIMIT=$(python -c "print($NUMBER_CPUS-1.0)")
