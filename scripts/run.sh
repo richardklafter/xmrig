86759ece5c7e2ef5ef509950e721e3529244d778
Richard Klafter <richard@zinc.com>
2018-01-08 02:11:58 -0800

    bug fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,5 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 echo "Detected $NUMCPU cores running with $NUMTHREADS threads"
-exec ../build/xmrig -t "$1" -o stratum+tcp://xdn-xmr.pool.minergate.com:45560 -u rpklafter@yahoo.com -p x
+exec ../build/xmrig -t "$1" -o stratum+tcp://xdn-xmr.pool.minergate.com:45790 -u rpklafter@yahoo.com -p x
 
