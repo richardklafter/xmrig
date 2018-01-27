ad8c5c19f7512dfd751353e401b1f8d61eb92b6b
Richard Klafter <richard@zinc.com>
2018-01-27 12:00:18 -0800

    added ability for cputtool_run.sh to shut down after x seconds

--- scripts/cputool_run.sh
+++ scripts/cputool_run.sh
@@ -6,5 +6,10 @@ NUMBER_CPUS=$(grep -c ^processor /proc/cpuinfo)
 NUMBER_OF_THREADS=$(python -c "from math import floor; print(int(floor($NUMBER_CPUS-$CURRENT_LOAD_AVG-1)))")
 LOAD_LIMIT=$(python -c "print($NUMBER_CPUS-1.0)")
 echo "Detected current load of $CURRENT_LOAD_AVG and $NUMBER_CPUS cpus. Running $NUMBER_OF_THREADS threads with load limit of $LOAD_LIMIT"
+SHUTDOWN_SECONDS="$1"
+if [ -n "${SHUTDOWN_SECONDS}" ]; then
+  echo "Shutting down the miner in ${SHUTDOWN_SECONDS} seconds"
+  ( sleep $SHUTDOWN_SECONDS; echo "Shutting down miner after ${SHUTDOWN_SECONDS} seconds"; kill $$ ) &
+fi
 exec cputool -v --load-limit "$LOAD_LIMIT" ./run.sh "$NUMBER_OF_THREADS"
 
