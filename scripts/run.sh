b83925cbce3b14dcb371349c0b80150177643180
Richard Klafter <richard@zinc.com>
2018-01-31 13:33:48 -0800

    update script

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,5 +5,5 @@ if [ "$#" -ne 1 ]; then
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
-exec ../build/xmrig -t "$1" -o stratum+tcp://xdn-xmr.pool.minergate.com:45790 -u rpklafter@yahoo.com -p x
+exec ../build/xmrig -t "$1" -o stratum+tcp://xdn-xmr.pool.minergate.com:45790 -u rpklafter@yahoo.com -p x $2
 
